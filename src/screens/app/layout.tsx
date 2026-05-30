630e28238290b6b717d986b817c839c8cba1d519
Pablo <pablolucio_@hotmail.com>
2026-05-30 20:53:18 -0300

    fix: adjusts logout modal

--- src/screens/app/layout.tsx
+++ src/screens/app/layout.tsx
@@ -362,7 +362,7 @@ export const DashboardLayout: React.FC<DashboardLayoutProps> = ({
           content="Deseja realmente sair da plataforma?"
           className="text-center text-gray-700 dark:text-gray-100 mb-6  text-[13px] md:text-[14px]"
         />
-        <div className="flex flex-col md:flex-row items-center justify-around w-full md:w-[480px]">
+        <div className="flex flex-col md:flex-row items-center justify-center w-full gap-4">
           <button
             onClick={handleToggleLogoutModal}
             className="text-black dark:text-white bg-gray-200 dark:bg-slate-700 px-4 py-2 rounded-lg text-sm w-[188px]"
